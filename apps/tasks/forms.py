00df2b8eb3c3c44e5216c3750483d613a4c66288
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-03-23 12:08:29 +0530

    feat(task): validate title input and prevent past date/time in task form

--- apps/tasks/forms.py
+++ apps/tasks/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.utils import timezone
+from datetime import datetime
 from .models import Task
 
 class TaskForm(forms.ModelForm):
@@ -11,3 +13,32 @@ class TaskForm(forms.ModelForm):
             'due_date': forms.DateInput(attrs={'type': 'date'}),
             'due_time': forms.TimeInput(attrs={'type': 'time'}),
         }
+    
+    def clean_title(self):
+        title = self.cleaned_data.get('title','').strip()
+        
+        if not title:
+            self.add_error('title', 'Title cannot be empty')
+        
+        if len(title) < 3:
+            self.add_error('title', 'Title must be at least 3 characters long.')
+        return title
+    
+    def clean(self):
+        cleaned_data =  super().clean()
+        due_date = cleaned_data.get('due_date')
+        due_time = cleaned_data.get('due_time')
+
+        if due_date:
+            today = timezone.localdate()
+
+            if due_date < today:
+                self.add_error('due_date', "Due date cannot be in the past")
+            
+            if due_date == today and due_time:
+                now_time = timezone.localtime().time()
+
+                if due_time < now_time:
+                    self.add_error('due_time', "Time must be in the Future.")
+
+        return cleaned_data
