6b4a331a1bc259d535047551583ee31ddc753c28
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-03-31 17:15:46 +0530

    fix: update URL patterns and configure CSRF trusted origins

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -4,10 +4,10 @@ from .views import (SignupView, VerifySignupOTP, ResendOTPView, LoginView,
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
-    path('verify_signup_otp/', VerifySignupOTP.as_view(), name='verify_signup_otp'),
-    path('resend-otp/', ResendOTPView.as_view(), name='resend_otp'),
+    path('verify/signup_otp/', VerifySignupOTP.as_view(), name='verify_signup_otp'),
+    path('resend/otp/', ResendOTPView.as_view(), name='resend_otp'),
     path('', LoginView.as_view(), name='login'),
-    path('verify_login_otp', VerifyLoginOTPView.as_view(), name='verify_login_otp'),
+    path('verify/login_otp', VerifyLoginOTPView.as_view(), name='verify_login_otp'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
     
